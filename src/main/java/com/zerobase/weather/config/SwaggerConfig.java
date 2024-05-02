79e90dc2eb4f080193cb59536b5b6bfdac9fac93
박이서 <jypark1011@thingspire.com>
2024-05-02 09:51:16 +0900

    스웨거 설정 수정

--- src/main/java/com/zerobase/weather/config/SwaggerConfig.java
+++ src/main/java/com/zerobase/weather/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.zerobase.weather.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
@@ -23,6 +25,13 @@ public class SwaggerConfig {
 
         return new OpenAPI()
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
-                .security(Arrays.asList(securityRequirement));
+                .security(Collections.singletonList(securityRequirement))
+                .info(apiInfo());
+    }
+    private Info apiInfo() {
+        return new Info()
+                .title("Weather Diary")
+                .description("날씨 일기 api 명세서")
+                .version("1.0.0");
     }
 }
